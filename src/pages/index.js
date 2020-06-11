222eb129ada44057ac58e2c0a7b6c2dc3a58185e
Zack McKenna <zackmckenna@gmail.com>
2020-06-11 16:49:56 -0400

    added styling to what text

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,8 +67,7 @@ const IndexPage = () => {
         </Row>
         <Row>
           <Col>
-            <p>In "From the Root to the Fruit: Portraits of Black Fathers and their Children" Ken McFarlane presents an ongoing documentary project that celebrates Black fatherhood while counteracting the false narrative of the absentee Black Father.  Originally presented by Traction Company on 41st Street and Haverford in Mantua and curated by Traction artist Sedakial Gebremedhin, this is a second public phase of this ongoing work.  Ken’s brilliant vision and images will be on display for two nights on the exterior walls of the Barnes Foundation, offering a shining counterpunch to a 400 year old system that has poisoned the image of Black Fatherhood in America.</p>
-          </Col>
+            <h2>In "From the Root to the Fruit: Portraits of Black Fathers and their Children" Ken McFarlane presents an ongoing documentary project that celebrates Black fatherhood while counteracting the false narrative of the absentee Black Father.</h2><h3>Originally presented by Traction Company on 41st Street and Haverford in Mantua and curated by Traction artist Sedakial Gebremedhin, this is a second public phase of this ongoing work.  Ken’s brilliant vision and images will be on display for two nights on the exterior walls of the Barnes Foundation, offering a shining counterpunch to a 400 year old system that has poisoned the image of Black Fatherhood in America.</h3>          </Col>
         </Row>
         <Row>
           <Col>
