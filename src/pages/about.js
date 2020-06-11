130ea19378d82cac220baf25682c57bf57f4e408
Zack McKenna <zackmckenna@gmail.com>
2020-06-11 18:21:59 -0400

    fixed linked on kens about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,8 +26,13 @@ const AboutPage = () => (
       </Row>
       <Row>
         <Col>
-          <h2>Ken McFarlane<span className='float-right'><a href='https://twitter.com/365ken'><TwitterIcon round={true} size={'30px'} /></a><a href='https://www.facebook.com/Ken-McFarlane-Photography-432087540877646/'><FacebookIcon round={true} size={'30px'}/></a>
+          <h2><a style={{ color: 'black'}} href="https://www.kenmcfarlanephoto.com/">Ken McFarlane</a><span className='float-right'><a href='https://twitter.com/365ken'><TwitterIcon round={true} size={'30px'} /></a><a href='https://www.facebook.com/Ken-McFarlane-Photography-432087540877646/'><FacebookIcon round={true} size={'30px'}/></a>
 </span></h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+
         </Col>
       </Row>
       <Row>
@@ -41,6 +46,11 @@ My most recent bodies of work have focused primarily on the production of visual
           <p>My mission is to produce a documentary portrait of West Philadelphia - a record of our truth, our stories, our histories, our futures. Images that are both evidence and symbol of our existence in this space and time. I do not have the privilege of creating art for art’s sake.  For my community, I am an advocate for our presence, our resistance, our strength, our beauty, our right to thrive and our right to remain a community.
 My most recent bodies of work have focused primarily on the production of visual manifestations and audio histories of Philadelphians as means of documentation for future generations.The intent of the work is to amplify the stories and images of the individual and community to preserve collective memory while encouraging future generations to realize, remember and re-examine the individual and collective image.
 </p>
+        </Col>
+      </Row>
+      <Row className='mb-2'>
+        <Col>
+          Visit <a href="https://www.kenmcfarlanephoto.com/">Ken McFarlane Photography</a> for more examples of his work and contact information.
         </Col>
       </Row>
     </Container>
