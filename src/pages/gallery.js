991229e698c43a738a986e94496b420c98be5be0
Zack McKenna <zackmckenna@gmail.com>
2020-06-12 12:10:39 -0400

    added more pictures and alternate gallery types

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,9 +1,14 @@
 import React from "react"
-import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+
+import Damon from '../images/gallery/Damon.jpg'
+import Erik from '../images/gallery/Erik.jpg'
+
+import DamonUncropped from '../images/gallery/Damon_uncrop.jpg'
+import ErikUncropped from '../images/gallery/Erik_uncrop.jpg'
 
 const GalleryPage = ({ data }) => (
   <Layout>
@@ -12,7 +17,34 @@ const GalleryPage = ({ data }) => (
       <Row>
         <Col className='mt-3'>
           <h1 style={{ fontFamily: 'Alfa Slab One', fontWeight: '400' }}>Gallery</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={Damon}></Image>
+        </Col>
+        <Col>
+          <p className='align-middle'>"To me, fatherhood is an everlasting imprint on people who are par of you and knowingly giving them a role model. It's one of the simple pleasures of life."</p>
+          <footer class="blockquote-footer">Damon</footer>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='my-auto'>
+        <p className='align-middle'>"My son doesn't have to follow in my footsteps, my job to make sure he is equipped with the legacy, standards and traditions that he can carry with him on whatever path he chooses."</p>
+          <footer class="blockquote-footer">Erik</footer>
+        </Col>
+        <Col>
+          <Image src={Erik}></Image>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={DamonUncropped}></Image>
+        </Col>
+        <Col>
+          <Image src={ErikUncropped}></Image>
+        </Col>
+        <Col>
         </Col>
       </Row>
     </Container>
