69a6fb85d1c726939942c1c91c2d60a52b97d2cc
Zack McKenna <zackmckenna@gmail.com>
2021-06-16 17:19:54 -0400

    removed old events

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react"
-import { Card, Container, Row, Col, Image } from 'react-bootstrap'
+import { Card, Container, Row, Col, Image } from "react-bootstrap"
 
 import Layout from "../components/layout"
-import standAgainst from '../images/standAgainst.jpeg'
-import wokeToVote from '../images/flyer_woke_to_vote.jpeg'
+import standAgainst from "../images/standAgainst.jpeg"
+import wokeToVote from "../images/flyer_woke_to_vote.jpeg"
 import SEO from "../components/seo"
-import './index.css'
+import "./index.css"
 
 const IndexPage = () => {
   const [toggle, setToggle] = useState(true)
@@ -19,16 +19,43 @@ const IndexPage = () => {
       <SEO title="Home" />
       <Container>
         <Row>
-          <Col className='mt-3 text-center'>
-              <h2 style={{ fontSize: '8vmin', fontWeight: '800', fontFamily: 'helvetica'}}>WeEmbraceFatherhood</h2>
+          <Col className="mt-3 text-center">
+            <h2
+              style={{
+                fontSize: "8vmin",
+                fontWeight: "800",
+                fontFamily: "helvetica",
+              }}
+            >
+              WeEmbraceFatherhood
+            </h2>
           </Col>
         </Row>
         <Row>
-          <Col className='text-center'>
+          <Col className="text-center">
             <h3>Mission statement:</h3>
             <p>
-We Embrace Fatherhood is a collective committed to the intentional transformation of families. We use public art, events, and an insistence on accountability as the catalyst for ongoing conversations about fostering strong male engagement in our communities. We turn these conversations into action, offering mentorship that guides fathers of all stripes to stand up and stand out. 
+              We Embrace Fatherhood is a collective committed to the intentional
+              transformation of families. We use public art, events, and an
+              insistence on accountability as the catalyst for ongoing
+              conversations about fostering strong male engagement in our
+              communities. We turn these conversations into action, offering
+              mentorship that guides fathers of all stripes to stand up and
+              stand out.
             </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Recent Past Events:</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <Image style={{ maxHeight: "500px" }} src={wokeToVote} />
+          </Col>
+          <Col>
+            <Image src={standAgainst} />
           </Col>
         </Row>
       </Container>
